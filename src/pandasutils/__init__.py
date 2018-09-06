12174c6b82a3a225ac18353e7c4ca69ce128d4fe
Fernando Siqueira <fernando.siqueira@dotgroup.com.br>
2018-09-05 21:50:04 -0300

    Adding multiprocessing apply

--- src/pandasutils/__init__.py
+++ src/pandasutils/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.3.0.0'
+__version__ = '0.4.0.0'
 
 from pandasutils.cli import main, format_columns_name, print_value_counts, get_field_from_df
 
--- src/pandasutils/cli.py
+++ src/pandasutils/cli.py
@@ -1,22 +1,12 @@
 """
-Module that contains the command line app.
 
-Why does this file exist, and why not put this in __main__?
+This module contains simple functions for pandas library.  
 
-  You might be tempted to import things from __main__ later, but that will cause
-  problems: the code will get executed twice:
-
-  - When you run `python -mpandasutils` python will execute
-    ``__main__.py`` as a script. That means there won't be any
-    ``pandasutils.__main__`` in ``sys.modules``.
-  - When you import __main__ it will get executed again (as a module) because
-    there's no ``pandasutils.__main__`` in ``sys.modules``.
-
-  Also see (1) from http://click.pocoo.org/5/setuptools/#setuptools-integration
 """
 import sys
 import pandas as pd
 from unidecode import unidecode
+import multiprocessing
 
 
 def format_columns_name(df_data):
@@ -67,22 +57,84 @@ def get_field_from_df(data, label, field, df_get_data, return_first_value=True, 
         label: the x label to compare
         field: the field to return
         df_get_data: dataframe to get value
-	return_first_value: boolean to return the first value or all values from DataFrame
-	null_return: Value to return if there is no value
+        return_first_value: boolean to return the first value or all values from DataFrame
+        null_return: Value to return if there is no value
 
     Returns:
-	the data from DataFrame
+    the data from DataFrame
 
     """
     try:
         if return_first_value:
-            return df_get_data.loc[df_get_data[label]==data,field].values[0]
+            return df_get_data.loc[df_get_data[label] == data, field].values[0]
 
         else:
-            return df_get_data.loc[df_get_data[label]==data,field].values
+            return df_get_data.loc[df_get_data[label] == data, field].values
 
     except Exception as e:
         return null_return
+
+
+def _apply_function(args):
+    """
+    Apply fuction
+    Args:
+        args: Dictionary of df_data, function and function parameters
+        
+    Returns:
+    DataFrame apply
+
+    """
+    df_data, function, kwargs = args
+    return df_data.apply(function, **kwargs)
+
+
+def multiprocessing_apply(df_data, function, **kwargs):
+    """
+    Function to use multiprocessors on pandas apply function
+    Args:
+        df_data: DataFrame for apply
+        function: function to apply on DataFrame
+        kwargs: function parameters
+        
+    Returns:
+    the result of the apply function
+
+    """
+    try:
+        num_cores = kwargs.pop('num_cores')
+    except Exception as e:
+        num_cores = 2
+
+    try:
+        verbose = kwargs.pop('verbose')
+    except Exception:
+        verbose = False
+
+    if verbose:
+        print('Creating multiprocessing with {} cores'.format(num_cores))
+    pool = multiprocessing.Pool(processes=num_cores)
+    df_list = []
+    step = int(df_data.shape[0] / num_cores)
+    for i in range(0, num_cores):
+        if i == num_cores - 1:
+            df_append = df_data[i * step:]
+        else:
+            df_append = df_data[i * step:(1 + i) * step]
+        if df_append.shape[0] > 0:
+            df_list.append(df_append)
+    if verbose:
+        print('Mapping process')
+    try:
+        res = pool.map(_apply_function, [(df, function, kwargs)
+                                         for df in df_list])
+        pool.close()
+        return pd.concat(list(res))
+    except Exception as e:
+        if verbose:
+            print('Error: {}'.formart(str(e)))
+        pool.terminate()
+
 
 def main(argv=sys.argv):
     """
@@ -92,7 +144,7 @@ def main(argv=sys.argv):
     Returns:
         int: A return code
 
-    Does stuff.
+
     """
     print(argv)
     return 0
