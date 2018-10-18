ac23c9c7b98f7b596d6d333593f8d26b26dd69ad
Fernando Siqueira <fernando.siqueira@dotgroup.com.br>
2018-10-18 10:01:00 -0300

    Update __init__ to include memusage function

--- src/pandasutils/__init__.py
+++ src/pandasutils/__init__.py
@@ -1,6 +1,6 @@
 __version__ = '0.6.0.0'
 
-from pandasutils.cli import main, format_columns_name, print_value_counts, get_field_from_df, join_dataframe_from_folder, split_unique, reduce_dataframe_size
+from pandasutils.cli import main, format_columns_name, print_value_counts, get_field_from_df, join_dataframe_from_folder, split_unique, reduce_dataframe_size, _mem_usage
 
 # Major
 # Minor
